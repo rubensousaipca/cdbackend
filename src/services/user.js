7e8f4b2d551c26215fa57fa7c9254e088c86c105
Ruben Sousa <rubensousayt@gmail.com>
2022-02-07 23:40:46 +0000

    Testes sobre parêmetros

--- src/services/user.js
+++ src/services/user.js
@@ -8,6 +8,14 @@ module.exports = (app) => {
         if (!user.email) return { error: 'O email é um atributo obrigatório!' };
         if (!user.password) return {error: 'A palavra-passe é um atributo obrigatório!'};
         if (!user.birthday) return {error: 'A data de nascimento é um atributo obrigatório!'};
+        if (!user.NIF) return {error: 'O NIF é um atributo obrigatório!'};
+        if (!user.BI) return {error: 'O BI é um atributo obrigatório!'};
+        if (!user.gender) return {error: 'O género é um atributo obrigatório!'};
+        if (!user.address) return {error: 'A morada é um atributo obrigatório!'};
+        if (!user.zip) return {error: 'O código postal é um atributo obrigatório!'};
+        if (!user.location) return {error: 'A localidade é um atributo obrigatório!'};
+        if (!user.admin) return {error: 'O admin é um atributo obrigatório!'};
+
 
         const userDb = await findAll({ email: user.email});
         if (userDb && userDb.length > 0) return { error: 'E-mail duplicado na base de dados!'};
--- test/routes/users.test.js
+++ test/routes/users.test.js
@@ -65,15 +65,15 @@ test('Test #5 - Inserir utilizador sem password', (done) => {
 
 test('Test #6 - Inserir utilizadores sem data de nascimento', () => {
     return request(app).post('/users')
-        .send({ 
-            name: 'Jose Ferreira',
+        .send({
+            name: 'Ruben Sousa',
             username: userrefresh,
             email: mail,
             NIF: nif,
             BI: bi,
             gender: 'masculine',
-            address: 'Rua D.Pedro V',
-            zip: '4715-422',
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
             location: 'Braga',
             password: '12345',
             admin: true,
@@ -83,6 +83,155 @@ test('Test #6 - Inserir utilizadores sem data de nascimento', () => {
             expect(res.body.error).toBe('A data de nascimento é um atributo obrigatório!');
         });
 });
+
+test('Test #7 - Inserir utilizadores sem NIF', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            gender: 'masculine',
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
+            location: 'Braga',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('O NIF é um atributo obrigatório!');
+        });
+});
+
+test('Test #8 - Inserir utilizadores sem BI', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            NIF: nif,
+            gender: 'masculine',
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
+            location: 'Braga',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('O BI é um atributo obrigatório!');
+        });
+});
+
+test('Test #9 - Inserir utilizadores sem género', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            NIF: nif,
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
+            location: 'Braga',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('O género é um atributo obrigatório!');
+        });
+});
+
+test('Test #10 - Inserir utilizadores sem morada', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            NIF: nif,
+            gender: 'masculine',
+            zip: '4760-702',
+            location: 'Braga',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('A morada é um atributo obrigatório!');
+        });
+});
+
+test('Test #10 - Inserir utilizadores sem código postal', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            NIF: nif,
+            gender: 'masculine',
+            address: 'Rua Alto das Laranjeiras 26',
+            location: 'Braga',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('O código postal é um atributo obrigatório!');
+        });
+});
+
+test('Test #10 - Inserir utilizadores sem localidade', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            NIF: nif,
+            gender: 'masculine',
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
+            password: '12345',
+            admin: true,
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('A localidade é um atributo obrigatório!');
+        });
+});
+
+test('Test #11 - Inserir utilizadores sem opção admin', () => {
+    return request(app).post('/users')
+        .send({
+            name: 'Ruben Sousa',
+            username: userrefresh,
+            email: mail,
+            birthday: '26/06/2000',
+            BI: bi,
+            NIF: nif,
+            gender: 'masculine',
+            address: 'Rua Alto das Laranjeiras 26',
+            zip: '4760-702',
+            location: 'Braga',
+            password: '12345',
+        })
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('O admin é um atributo obrigatório!');
+        });
+});
+
+
 
 /*
 test('Test #6 - Inserir utilizadores', () => {
